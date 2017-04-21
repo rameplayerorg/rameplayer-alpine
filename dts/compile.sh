737c9b1379188dd15c1f885673ce32dae2169c78
Jetro Lauha <jlauha@gmail.com>
2017-04-21 17:49:50 +0000

    updated dts helper compile script for newer result filename format

--- dts/compile.sh
+++ dts/compile.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 DTC=~/git/dtc/dtc
 
-$DTC -@ -I dts -O dtb rame-cid1-r1kbd-overlay.dts > rame-cid1-r1kbd-overlay.dtb
-$DTC -@ -I dts -O dtb rame-cid2-textlcd-overlay.dts > rame-cid2-textlcd-overlay.dtb
-$DTC -@ -I dts -O dtb rame-cid3-r2kbd-overlay.dts > rame-cid3-r2kbd-overlay.dtb
-$DTC -@ -I dts -O dtb rame-cid5-audio-overlay.dts > rame-cid5-audio-overlay.dtb
-$DTC -@ -I dts -O dtb rame-cid4-lcd-overlay.dts > rame-cid4-lcd-overlay.dtb
-$DTC -@ -I dts -O dtb rame-cid6-rtc-overlay.dts > rame-cid6-rtc-overlay.dtb
-$DTC -@ -I dts -O dtb rame-cid7-extbuttons-overlay.dts > rame-cid7-extbuttons-overlay.dtb
+$DTC -@ -I dts -O dtb rame-cid1-r1kbd-overlay.dts > rame-cid1-r1kbd.dtbo
+$DTC -@ -I dts -O dtb rame-cid2-textlcd-overlay.dts > rame-cid2-textlcd.dtbo
+$DTC -@ -I dts -O dtb rame-cid3-r2kbd-overlay.dts > rame-cid3-r2kbd.dtbo
+$DTC -@ -I dts -O dtb rame-cid5-audio-overlay.dts > rame-cid5-audio.dtbo
+$DTC -@ -I dts -O dtb rame-cid4-lcd-overlay.dts > rame-cid4-lcd.dtbo
+$DTC -@ -I dts -O dtb rame-cid6-rtc-overlay.dts > rame-cid6-rtc.dtbo
+$DTC -@ -I dts -O dtb rame-cid7-extbuttons-overlay.dts > rame-cid7-extbuttons.dtbo
